7600c4fec7f2f6103658f8d7077b57d47404ea33
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-10-16 16:12:46 -0500

    Cambiando la url del backend

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 // const BACKEND_URL = 'https://bsale-backend.onrender.com/api';
-const BACKEND_URL = "http://localhost:3001/api";
 
 export const getProducts = async () => {
   const response = await fetch(`${BACKEND_URL}/products`);
