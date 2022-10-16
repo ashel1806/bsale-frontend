d1770430ea522fdec47c9360791f2fcbd169f87f
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-10-16 17:01:14 -0500

    Implementando busqueda de productos

--- src/api.js
+++ src/api.js
@@ -1,9 +1,21 @@
-// const BACKEND_URL = 'https://bsale-backend.onrender.com/api';
+const BACKEND_URL = 'https://bsale-backend.onrender.com/api';
 
-export const getProducts = async () => {
-  const response = await fetch(`${BACKEND_URL}/products`);
+export const getProducts = async ({
+  search = '',
+  page = 0,
+  limit = 10,
+  category = '',
+} = {}) => {
+  let query = '?';
+  if (search) query += `search=${search}&`;
+  if (page) query += `page=${page}&`;
+  if (limit) query += `limit=${limit}&`;
+  if (category) query += `category=${category}&`;
+
+
+  const response = await fetch(`${BACKEND_URL}/products/${query}`);
   const { products } = await response.json();
-  console.log(products);
+  // console.log(products);
 
   return products;
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import { getProducts } from "./api.js";
 import Product from "./components/Product.js";
 
 const productsContainer = document.querySelector("#products");
+const searchForm = document.querySelector("#searchForm");
+
+let filter = {};
+
+async function fetchProducts() {
+  const products = await getProducts(filter);
+  console.log(products);
+  productsContainer.innerHTML = products.map(Product).join("");
+};
+
+searchForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  filter.search = e.target.search.value;
+  fetchProducts();
+});
 
 const App = async () => {
-  const products = await getProducts();
-
-  productsContainer.innerHTML = `${products.map((product) => Product(product)).join("")}`;
+  await fetchProducts();
 }
 
 App();
